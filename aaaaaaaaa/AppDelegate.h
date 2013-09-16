3517dfcaf1aa52300a20d4ed0f3330142c2fed3a
ma <maxmyers@new-host-3.home>
2013-09-15 23:45:14 -0700

    Initial Commit

---
+++ aaaaaaaaa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  aaaaaaaaa
+//
+//  Created by max myers on 9/15/13.
+//  Copyright (c) 2013 Pop Shorts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ aaaaaaaaa/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  aaaaaaaaa
+//
+//  Created by max myers on 9/15/13.
+//  Copyright (c) 2013 Pop Shorts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
