523d01fc5c9d4ad5fc98dee168dc48cbd98abe79
ma <maxmyers@new-host-3.home>
2013-09-15 23:51:40 -0700

    added files

---
+++ aaaaaaaaa/view1.h
@@ -0,0 +1,13 @@
+//
+//  view1.h
+//  aaaaaaaaa
+//
+//  Created by max myers on 9/15/13.
+//  Copyright (c) 2013 Pop Shorts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface view1 : UIView
+
+@end
---
+++ aaaaaaaaa/view1.m
@@ -0,0 +1,31 @@
+//
+//  view1.m
+//  aaaaaaaaa
+//
+//  Created by max myers on 9/15/13.
+//  Copyright (c) 2013 Pop Shorts. All rights reserved.
+//
+
+#import "view1.h"
+
+@implementation view1
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
